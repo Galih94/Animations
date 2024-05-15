ab24ae1b84be976460c0977b6a255b07c2f18fa7
galih <samudragalih94@gmail.com>
2024-05-15 11:58:49 +0700

    wip initial push

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Galih Samudra on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
