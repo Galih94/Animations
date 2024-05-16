258122705dc0ae88b1e7459d75e90c2ca0c80517
galih <samudragalih94@gmail.com>
2024-05-16 12:39:12 +0700

    feat(UI): create 3d ritation animation using `.rotation3DEffect` and `withAnimation` on change scale animationAmount

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     var body: some View {
-        print(animationAmount)
         return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                .repeatCount(3)
-            ), in: 1...10)
-            Spacer()
             Button("Tap me") {
-                animationAmount += 1
+                withAnimation {
+                    animationAmount += 360
+                }
             }
             .padding(40)
             .background(.red)
             .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
+            .clipShape(.ellipse)
+            .rotation3DEffect(
+                .degrees(animationAmount), axis: (x: 0.0, y: 1.0, z: 0.0)
+            )
         }
     }
 }
