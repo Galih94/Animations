e19cf5b81bb644093490c8f6e2c2f971c8b2f89c
galih <samudragalih94@gmail.com>
2024-05-16 16:16:26 +0700

    feat(UI): set animation nil to remove animation change color

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             .frame(width: 200, height: 200)
             .background(enabled ? .blue : .red)
             .foregroundStyle(.white)
-            .animation(.default, value: enabled)
+            .animation(nil, value: enabled)
             .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
             .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
         }
