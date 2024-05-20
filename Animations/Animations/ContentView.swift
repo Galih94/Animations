87897c38dcefd43237d029624fd5b40004420ea9
galih <samudragalih94@gmail.com>
2024-05-20 15:24:56 +0700

    feat(UI): add ecample animation shape using `transition`

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,30 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Helllo SwiftUI")
-    @State private var dragAmount = CGSize.zero
-    @State private var enable = false
+    @State private var isShowing = false
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { index in
-                Text(String(letters[index]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enable ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(index) / 20), value: dragAmount)
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowing.toggle()
+                }
+            }
+            
+            if isShowing {
+                
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged({ value in
-                    dragAmount = value.translation
-                })
-                .onEnded({ _ in
-                    dragAmount = .zero
-                    enable.toggle()
-                })
-        )
     }
 }
 
